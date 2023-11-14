4b05dd6d444c93167f74f0c7da5e407faf4055f7
pedromourax <pedrolucas.moura@hotmail.com>
2023-11-14 16:30:00 -0300

    feat: remocao de comentarios

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const caminhoDoProjeto = path.dirname(argv[1])
 
 const diretorioArquivoBruto = path.join(caminhoDoProjeto, "Arquivo Bruto")
 
-// SELECIONA O ARQUIVO MAIS RECENTE DO DIRETÓRIO 'Arquivo Bruto'
 const getMaisRecente = (dir) => {
     const arquivos = fs.readdirSync(dir)
     return underscore.max(arquivos, function (a) {
@@ -29,12 +28,10 @@ if (!fs.existsSync(redimensionados)) {
 
 const tamanhos = [128, 64, 32, 16]
 
-// CASO ESTIVER CRIADO OS DIRETORIOS E NÃO TIVER INSERIDO AS IMAGENS... 
 if (typeof getMaisRecente(diretorioArquivoBruto) === "number") {
     console.log(`Insira as imagens em -> '${diretorioArquivoBruto}' e execute novamente... `)
 }
 
-// CASO ESTIVER INSERIDO A IMAGEM NA PASTA 'Arquivo Bruto', IRÁ REDIMENSIONAR
 else if (typeof getMaisRecente(diretorioArquivoBruto) === "string") {
     const [nomeDoArquivo, extensao] = path.basename(getMaisRecente(diretorioArquivoBruto)).split(".")
     const arquivoMaisRecente = path.join(diretorioArquivoBruto, `${getMaisRecente(diretorioArquivoBruto)}`)
@@ -48,7 +45,6 @@ else if (typeof getMaisRecente(diretorioArquivoBruto) === "string") {
     console.log("Arquivo redimensionado!")
 }
 
-// CASO AINDA NÃO TENHA CRIADO OS DIRETÓRIOS
 else {
     console.log(`Diretórios criados! Insira as imagens em -> '${diretorioArquivoBruto}' e execute novamente... `)
 }
